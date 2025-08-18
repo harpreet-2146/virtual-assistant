c178c56418286cc5606e5c4a6b92b737f15f76c1
harpreet-2146 <harpreetk2146@gmail.com>
2025-08-18 23:27:01 +0530

    finally auth is working fine

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
+dotenv.config()
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 dotenv.config()
 
 const app=express()
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
