28cbf473dc1e8eb53a758f9494185c52c2bbc1c6
harpreet-2146 <harpreetk2146@gmail.com>
2025-08-20 05:59:23 +0530

    userRoutes updated

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,5 +2,5 @@ import express from "express"
 import {getCurrentUser} from "../controllers/user.controllers.js"
 const userRouter=express.Router()
 
-authRouter.get("/current",getCurrentUser)
+userRouter.get("/current",getCurrentUser)
 export default userRouter
