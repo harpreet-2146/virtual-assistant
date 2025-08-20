1be35f401fc3125f9582b18df992c93210672aad
harpreet-2146 <harpreetk2146@gmail.com>
2025-08-20 06:29:21 +0530

    cloudinary done

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,9 +1,28 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import axios from 'axios'
 export const userDataContext=createContext()
 function UserContext({children}) {
     const serverUrl="http://localhost:8000"
+    const [userData,setUserData]=useState(null)
+
+    const handleCurrentUser=async()=>{
+      try {
+        const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
+        setUserData(result.data)
+        console.log(result.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    useEffect(()=>{
+      handleCurrentUser()
+    },[])
+  
+
     const value={
-          serverUrl
+          serverUrl,
+          userData,
+          setUserData
     }
   return (
     <div>
