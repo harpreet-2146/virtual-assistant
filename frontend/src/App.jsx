073e6fe90bc3391f99e66a8ada12144fe23331f8
harpreet kaur gothra <harpreetk2146@gmail.com>
2025-12-31 01:47:56 +0530

    refactor: simplify main.jsx and App.jsx, remove auth dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
 import Assistant from './pages/Assistant'
 
@@ -9,8 +7,6 @@ function App() {
     return (
         <Routes>
             <Route path='/' element={<Assistant />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/signin' element={<SignIn />} />
             <Route path='/customize' element={<Customize />} />
         </Routes>
     )
