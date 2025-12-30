08c20663cac2d040c357bc7bdb6fd2cb7385114b
harpreet kaur gothra <harpreetk2146@gmail.com>
2025-12-31 01:29:40 +0530

    refactor: update routes to include Assistant page as home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
+import Assistant from './pages/Assistant'
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/customize' element={<Customize/>} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path='/' element={<Assistant />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/customize' element={<Customize />} />
+        </Routes>
+    )
 }
 
 export default App
